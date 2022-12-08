895db7e6b5fdb9462dc162fd280f142462419b14
vrijraj <vrijraj2396@gmail.com>
2022-12-08 13:20:59 +0530

    store updated

--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    users: {},
   },
   getters: {
+    GET_USERS: (state)=>state.users
   },
   mutations: {
+    UPDATE_USERS: (state, new_value)=> state.users = new_value
   },
   actions: {
+    GET_ALL_USERS: ({commit}, val)=>{
+      console.log(val);
+      fetch('https://api.github.com/users').then(res=>res.json()).then(data=>{
+        commit('UPDATE_USERS', data)
+      }).catch(err=>{
+        console.log(err);
+      })
+    }
   },
   modules: {
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>Hello</p>
+    <!-- <h1>{{$store.getters.GET_USERS}}</h1> -->
+    <!-- <h3>{{b}}</h3> -->
+    <!-- <p>{{c}}</p> -->
+    <table border="1">
+      <tr>
+        <td>User Name</td>
+        <td>Image</td>
+      </tr>
+      <tr v-for="(item, index) in c" :key="index">
+        <td>{{item.login}}</td>
+        <td>{{item.avatar_url}}</td>
+      </tr>
+    </table>
+    <button @click="updateValue">Update</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { mapGetters } from 'vuex'
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data:()=>({
+    b: 0
+  }), 
+  computed:{
+    ...mapGetters({c: 'GET_USERS'})
+    
+  },  
+  mounted(){
+    // this.b = this.$store.getters.GET_VALUE
+  },
+  methods:{
+    updateValue(){
+      // this.$store.commit('UPDATE_USERS', 45)
+      this.$store.dispatch('GET_ALL_USERS', 78)
+    }
   }
 }
 </script>
